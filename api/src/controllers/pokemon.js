36442738df70196bb61e7bb49baf343e03e18a57
limperatri <lautaro.d.gutierrez@gmail.com>
2022-09-22 14:39:21 -0300

    Refactor back-end

---
+++ api/src/controllers/pokemon.js
@@ -0,0 +1,309 @@
+const { Pokemon, Types } = require('../db');
+class PokemonController {
+
+    async findAllPokemon(req, res) {
+        try {
+            const { name, typeFilter, order, direction } = req.query;
+            if (name) { //SEARCH BY NAME
+                const findByName = await Pokemon.findOne({ where: { name: name } })
+                if (!findByName) {
+                    return res.status(404).send({
+                        success: false,
+                        message: `Name ${name} doesn't exist`
+                    })
+                }
+                return res.send({
+                    success: true,
+                    data: findByName
+                })
+            }
+            let allPoke;
+            if (typeFilter) { //IF FILTER BY TYPE
+                if (order) { //IF FILTER BY TYPE AND ORDER
+                    const filterTypeOrder = await Pokemon.findAll({
+                        order: [[`${order}`, direction]],
+                        include: {
+                            model: Types,
+                            where: { 'name': typeFilter },
+                            attributes: ['name'],
+                            through: { attributes: [] },
+                        }
+                    })
+                    if (!filterTypeOrder) return res.send({
+                        success: false,
+                        message: `No pokemon with type ${typeFilter}`
+                    });
+                    return res.send({
+                        success: true,
+                        data: filterTypeOrder
+                    })
+                } //IF  FILTER BY TYPE ONLY
+                const filterTypeOnly = await Pokemon.findAll({
+                    include: {
+                        model: Types,
+                        where: { 'name': typeFilter },
+                        attributes: ['name'],
+                        through: { attributes: [] },
+                    }
+
+                })
+                if (!filterTypeOnly) return res.send({
+                    success: false,
+                    message: `No pokemon with type ${typeFilter}`
+                });
+                return res.send({
+                    success: true,
+                    data: filterTypeOnly
+                })
+
+
+            }
+            if (order) { //IF ORDER ONLY
+                console.log(req.query)
+                allPoke = await Pokemon.findAll({
+                    order: [[`${order}`, direction]],
+                    include: {
+                        model: Types,
+                        attributes: ['name'],
+                        through: { attributes: [] },
+                    }
+                })
+            } else { //IF NO FILTER/ORDER PARAMETERS WERE SENT, IT RETURNS ALL POKEMON IN DATA BASE
+                allPoke = await Pokemon.findAll({
+                    include: {
+                        model: Types,
+                        attributes: ['name'],
+                        through: { attributes: [] },
+                    }
+                });
+            }
+            if (!allPoke?.length) return res.send({
+                success: false,
+                message: 'No Pokemon have been found'
+            });
+
+            res.send({
+                success: true,
+                data: allPoke
+            })
+        } catch (err) {
+            res.send({
+                error: true,
+                message: err.message
+            })
+        }
+    }
+
+    async createPokemon(req, res) {
+        try {
+            const { name, hp, attack, defense, speed, height, weight, image, types } = req.body
+
+            let arrType = []
+            if (!types?.length) {
+                return res.status(400).json({
+                    succes: false,
+                    message: `Choose at least one type`
+                })
+            }
+
+            const exists = await Pokemon.findOne({ where: { name: req.body.name } })
+
+            if (exists) return res.send({
+                succes: false,
+                message: `The name ${name} already exists`
+            })
+
+            types.map((e) => arrType.push({ name: e }))
+
+            const newPoke = await Pokemon.create({
+                name: name,
+                hp: hp,
+                attack: attack,
+                defense: defense,
+                speed: speed,
+                height: height,
+                weight: weight,
+                createdDb: true,
+                img: image
+                    ? image
+                    : 'https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif',
+            })
+            let typeDb = await Types.findAll({ where: { name: arrType[0].name } })
+            newPoke.addType(typeDb)
+            if (arrType[1]) {
+                let typeDb2 = await Types.findAll({ where: { name: arrType[1].name } })
+                newPoke.addType(typeDb2)
+            }
+
+            res.status(201).send({
+                succes: true,
+                message: 'Pokemon created successfully!'
+            })
+        } catch (err) {
+            res.status(400).send({
+                error: true,
+                message: err.message
+            })
+        }
+    }
+
+    async deletePokemon(req, res) {
+        const { id } = req.params
+        try {
+            const toDelete = await Pokemon.findOne({ where: { id: id } })
+            if (toDelete) {
+                await Pokemon.destroy({ where: { id: id } })
+                return res.send({
+                    success: true,
+                    message: 'Pokemon Deleted successfully'
+                })
+            }
+            res.send({
+                success: false,
+                message: `The Pokemon id  ${id}  doen't exist `
+            })
+        } catch (err) {
+            res.send({
+                error: true,
+                message: err.message
+            })
+        }
+    }
+
+    async getPokemonById(req, res) {
+        try {
+            const { id } = req.params
+            const findPoke = await Pokemon.findByPk(id, {
+                include: {
+                    model: Types,
+                    attributes: ['name'],
+                    through: { attributes: [] },
+                }
+            });
+            if (!findPoke) {
+                return res.status(404).send({
+                    success: false,
+                    message: 'Pokemon not found'
+                })
+            }
+
+            res.send({
+                succes: true,
+                data: findPoke
+            })
+
+        } catch (error) {
+            res.send({
+                error: true,
+                message: error.message
+            })
+        }
+    }
+
+    async updatePokemon(req, res) {
+        let { id } = req.params
+        let { name, hp, attack, defense, speed, height, weight, img } = req.body
+        console.log(name)
+        if (!req.body) {
+            return res.send({
+                success: false,
+                message: 'No information has been received'
+            })
+        }
+        try {
+            if (name) {
+                const checkName = await Pokemon.findOne({ where: { name: name } });
+                if (checkName) return res.send({
+                    success: false,
+                    message: `The pokemon name ${name} already exists`
+                })
+            }
+            let find = await Pokemon.findOne({ where: { id: id } })
+            if (!find) {
+                return res.status(404).send({
+                    success: false,
+                    message: `The pokemon id "${id}", doesn't exist`
+                })
+            }
+
+            await Pokemon.update(
+
+                {
+                    name: name ? name : find.name,
+                    hp: hp ? hp : find.hp,
+                    attack: attack ? attack : find.attack,
+                    defense: defense ? defense : find.defense,
+                    speed: speed ? speed : find.speed,
+                    height: height ? height : find.height,
+                    weight: weight ? weight : find.weight,
+                    img: img ? img : find.img,
+                },
+                { where: { id: id } }
+            )
+
+            return res.send({
+                success: true,
+                message: 'Updated successfully'
+            })
+
+        } catch (err) {
+            res.send({
+                error: true,
+                message: err.message
+            })
+        }
+    }
+
+    async filter(req, res) {
+        try {
+            if (!req.body.filters) {
+                res.status(400).send({
+                    success: false,
+                    message: 'No filters were sent'
+                })
+            }
+            const { filters } = req.body;
+            if (filters.type) {
+                var filterType = await Pokemon.findAll({
+                    include: {
+                        model: Types,
+                        where: { name: filters.type },
+                        attributes: ['name'],
+                    },
+                })
+                if (filters.order) {
+                    filterType.sort((a, b) => {
+                        a[filters.order] - b[filters.order]
+                    })
+                }
+                return res.send({
+                    success: true,
+                    data: filterType
+                })
+            }
+
+            var allPokemon = await Pokemon.findAll({
+                include: {
+                    model: 'Types',
+                    attributes: ['name'],
+                    through: { attributes: [] }
+                }
+            })
+
+            allPokemon.sort((a, b) => {
+                a[filters.order] - b[filters.order]
+            })
+
+        } catch (error) {
+            console.log(error)
+            res.status(400).send({
+                error: true,
+                message: error.message
+            })
+        }
+    }
+}
+
+
+
+module.exports = new PokemonController();
--- api/src/routes/Pokemons.js
+++ api/src/routes/Pokemons.js
@@ -1,184 +1,63 @@
 const router = require('express').Router()
-const { Op } = require('sequelize')
 const { Pokemon, Types } = require('../db')
-const { setOrder, filters } = require('../Middlewares/middleware')
+const { check, query } = require('express-validator');
+const pokemon = require('../controllers/pokemon');
+const validateField = require('../Middlewares/validateField')
 
-//********************************************************************************************************************************************//
-//*********************************************************************************************************************************//
-//********************************************************************************************************************************************//
+router.get('/', pokemon.findAllPokemon);
 
-router.get('/', async (req, res) => {
-  let { name, typeFilter, order, created } = req.query
-  try {
-    if (created !== 'default' && created == true) {
-      const createdInDb = await Pokemon.findAll({
-        where: { createdDb: created },
-      })
-      if (!createdInDb.length) {
-        return res.send({ msg: "You haven't created any new pokemon yet" })
-      }
-      return res.send(createdInDb)
-    }
+//
+router.post('/', [
+  check('name', 'name Must not be empty').not().isEmpty(),
+  check('name', 'name Must be a string').isString(),
 
-    if (name && name !== '') {
-      //find by name
-      const poke = await Pokemon.findAll({
-        where: {
-          name: { [Op.iLike]: `%${name}%` },
-        },
-        include: {
-          model: Types,
-          attributes: ['name'],
-          through: { attributes: [] },
-        },
-      })
-      if (poke?.length) return res.status(200).send(poke)
-      else return res.status(404).send(`The pokemon "${name}" doesn't exist.`)
-    }
+  check('hp', 'hp must not be empty').not().isEmpty(),
+  check('hp', 'hp must be an integer').isInt({ min: 1 }),
 
-    if (typeFilter !== 'default' && typeFilter) {
-      const filteredPoke = await filters(
-        typeFilter,
-        order !== 'default' && order ? order : null,
-      )
-      return res.send(filteredPoke)
-    }
-    var allPokeNoMods = await Pokemon.findAll({
-      include: {
-        model: Types,
-        attributes: ['name'],
-        through: { attributes: [] },
-      },
-    })
-    if (order !== 'default' && order) {
-      allPokeNoMods = setOrder(allPokeNoMods, order)
-    }
-    res.send(allPokeNoMods) // return All
-  } catch (err) {
-    console.log('here be error', err.message)
-    res.send({ msg: err.message })
-  }
-})
+  check('attack', 'attack must not be empty').not().isEmpty(),
+  check('attack', 'attack must be an integer').isInt({ min: 1 }),
 
-//********************************************************************************************************************************************//
-//*********************************************************************************************************************************//
-//********************************************************************************************************************************************//
+  check('defense', 'defense must not be empty').not().isEmpty(),
+  check('defense', 'defense must be an integer').isInt({ min: 1 }),
 
-router.post('/create', async (req, res) => {
-  const { name, hp, attack, defense, speed, height, weight, image } = req.body
-  if (!name || !hp || !attack || !defense || !speed || !height || !weight) {
-    return res.status(400).json({
-      info: `Theres a missing value`,
-    })
-  }
-  let arrType = []
-  req.body.types.map((e) => arrType.push({ name: e }))
-  if (!arrType.length) {
-    return res.status(400).json({ info: `Choose at least one type` })
-  }
+  check('speed', 'speed must not be empty').not().isEmpty(),
+  check('speed', 'speed must be an integer').isInt({ min: 1 }),
 
-  const exists = await Pokemon.findOne({ where: { name: req.body.name } })
+  check('height', 'height must not be empty').not().isEmpty(),
+  check('height', 'height must be an integer').isInt({ min: 1 }),
 
-  if (exists) return res.json({ info: 'This pokemons already exists!' })
-  try {
-    const newPoke = await Pokemon.create({
-      name: req.body.name,
-      hp: req.body.hp,
-      attack: req.body.attack,
-      defense: req.body.defense,
-      speed: req.body.speed,
-      height: req.body.height,
-      weight: req.body.weight,
-      createdDb: true,
-      img: image
-        ? image
-        : 'https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif',
-    })
-    let typeDb = await Types.findAll({ where: { name: arrType[0].name } })
-    newPoke.addType(typeDb)
-    if (arrType[1]) {
-      let typeDb2 = await Types.findAll({ where: { name: arrType[1].name } })
-      newPoke.addType(typeDb2)
-    }
+  check('weight', 'weight must not be empty').not().isEmpty(),
+  check('weight', 'weight must be an integer').isInt({ min: 1 }),
 
-    res.status(201).send({ msg: 'Pokemon created successfully!' })
-  } catch (err) {
-    console.log(err)
-  }
-})
+  check('image', 'image must be an integer').isString(),
+  validateField
+],
+  pokemon.createPokemon
+);
 
-//********************************************************************************************************************************************//
-//*********************************************************************************************************************************//
-//********************************************************************************************************************************************//
+router.put('/:id', [
+  check('name', 'name Must be a string').optional().isString(),
 
-router.put('/modify/:id', async (req, res) => {
-  let { id } = req.params
-  let { name, hp, attack, defense, speed, height, weight, img } = req.body
-  try {
-    let find = await Pokemon.findOne({ where: { id: id } })
-    if (find) {
-      await Pokemon.update(
-        {
-          name: name ? name : find.name,
-          hp: hp ? hp : find.hp,
-          attack: attack ? attack : find.attack,
-          defense: defense ? defense : find.defense,
-          speed: speed ? speed : find.speed,
-          height: height ? height : find.height,
-          weight: weight ? weight : find.weight,
-          img: img ? img : find.img,
-        },
-        { where: { id: id } },
-      )
+  check('hp', 'hp must be an integer').optional().isInt({ min: 1 }),
 
-      return res.send({ msg: 'Updated successfully' })
-    }
-    res.send({ msg: "This Pokemon doen't exist" })
-  } catch (err) {
-    console.log('This be the message', err.message)
-    res.send({ msg: err.message })
-  }
-})
+  check('attack', 'attack must be an integer').optional().isInt({ min: 1 }),
 
-//********************************************************************************************************************************************//
-//*********************************************************************************************************************************//
-//********************************************************************************************************************************************//
+  check('defense', 'defense must be an integer').optional().isInt({ min: 1 }),
 
-router.get('/:id', async (req, res) => {
-  try {
-    let { id } = req.params
-    const findIt = await Pokemon.findOne({
-      where: { id: id },
-      include: {
-        model: Types,
-        attributes: ['name'],
-        through: { attributes: [] },
-      },
-    })
-    res.send(findIt)
-  } catch (err) {
-    console.log(err.message)
-    res.send({ msg: err.msg })
-  }
-})
+  check('speed', 'speed must be an integer').optional().isInt({ min: 1 }),
 
-//********************************************************************************************************************************************//
-//*********************************************************************************************************************************//
-//********************************************************************************************************************************************//
+  check('height', 'height must be an integer').optional().isInt({ min: 1 }),
 
-router.delete('/delete/:id', async (req, res) => {
-  const { id } = req.params
-  try {
-    const toDelete = await Pokemon.findOne({ where: { id: id } })
-    if (toDelete) {
-      await Pokemon.destroy({ where: { id: id } })
-      return res.send({ msg: 'Pokemon Deleted successfully' })
-    }
-    res.send({ msg: `The Pokemon id  ${id}  doen't exist ` })
-  } catch (err) {
-    console.log({ msg: err.message })
-    res.send({ msg: err.message })
-  }
-})
+  check('weight', 'weight must be an integer').optional().isInt({ min: 1 }),
+
+  check('image', 'image must be an integer').optional().isString(),
+  validateField
+], pokemon.updatePokemon);
+
+router.get('/:id', pokemon.getPokemonById);
+
+router.delete('/:id', pokemon.deletePokemon);
+
+router.post('/filter', pokemon.filter);
 
 module.exports = router
